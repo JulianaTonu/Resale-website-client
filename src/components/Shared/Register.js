28f8abc94721c219f77dea7a4daa487549261769
unknown <julianatonu@gmail.com>
2022-11-23 23:12:15 +0600

    Added register

--- src/components/Shared/Register.js
+++ src/components/Shared/Register.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+
+    const handleSignup = event =>{
+        event.preventDefault()
+        const form =event.target
+        const name=form.name.value
+        
+        const email =form.email.value;
+        const password=form.password.value
+        console.log(name,email,password)
+    }
+
     return (
-        <div>
-            
+        <div className="hero my-18 ">
+        <div className="hero-content ">
+       
+          <div className="card flex-shrink-0 shadow-lg bg-base-100 my-10 py-3">
+          <h1 className="text-4xl text-center  font-bold">Register </h1>
+            <form onSubmit={handleSignup} className="card-body">
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name='name' placeholder="your name" className="input input-bordered" />
+              </div>
+
+             
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                <label className="label">
+                  <a href className="label-text-alt link link-hover" >Forgot password?</a>
+                </label>
+              </div>
+
+
+              <div className="form-control mt-6">
+                <input type="submit" value="Register" className="btn bg"/>
+           
+               
+              </div>
+            </form>
+
+            <p className=' text-center '>Already have an account?<Link className='text-purple-600 font-bold' to='/login'>Login</Link></p>
+          </div>
         </div>
+      </div>
     );
 };
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArMxTPe3StAbL7yKDG52bU_zNbbT3fMkw",
+  authDomain: "resale-website-8d51b.firebaseapp.com",
+  projectId: "resale-website-8d51b",
+  storageBucket: "resale-website-8d51b.appspot.com",
+  messagingSenderId: "161262694510",
+  appId: "1:161262694510:web:4c252adec63597dda9f67f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
